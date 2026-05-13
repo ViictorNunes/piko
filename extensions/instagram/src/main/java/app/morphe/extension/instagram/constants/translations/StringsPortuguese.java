c99c21dc3b566c4a4b6dc3f33ff4bc3b1a3d32b3
votsz <joaovictorvalentinanunes@gmail.com>
2026-05-12 23:05:27 -0300

    Brazilian Portuguese translation

---
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/StringsPortuguese.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2026 piko <https://github.com/crimera/piko>
+ *
+ * This file is part of piko.
+ *
+ * Any modifications, derivatives, or substantial rewrites of this file
+ * must retain this copyright notice and the piko attribution
+ * in the source code and version control history.
+ */
+
+
+package app.morphe.extension.instagram.constants.translations;
+
+public class DefaultStrings {
+    public static String PIKO_SETTINGS_TITLE = "Configurações do Piko";
+
+    public static String CATEGORY_ADS = "Anúncios";
+    public static String DISABLE_ADS = "Desativar anúncios";
+    public static String HIDE_SUGEESTED_CONTENT = "Ocultar conteúdo sugerido";
+    public static String HIDE_SUGEESTED_CONTENT_DESC = "Oculta stories, reels e threads sugeridos (posts sugeridos ainda serão mostrados).";
+
+    public static String CATEGORY_DEV_OPTIONS = "Opções de desenvolvedor";
+    public static String ENABLE_DEV_OPTIONS = "Ativar opções de desenvolvedor";
+    public static String ENABLE_DEV_OPTIONS_DESC = "Desbloqueia as opções de desenvolvedor ao segurar o ícone da home";
+    public static String DIRECTLY_OPEN_METACONFIG = "Abrir MetaConfig overrides diretamente";
+    public static String DIRECTLY_OPEN_METACONFIG_DESC = "Abre diretamente o MetaConfig overrides ao segurar o ícone da home (requer opções de desenvolvedor ativadas)";
+    public static String ENABLE_EMP_OPTIONS = "Ativar Menu de funcionário";
+    public static String ENABLE_EMP_OPTIONS_DESC = "Desbloqueia todas as opções de funcionário usadas para testes";
+    public static String ALLOW_USER_NETWORK_CERTIFICATE = "Permitir certificado de rede do usuário";
+    public static String ALLOW_USER_NETWORK_CERTIFICATE_DESC = "Permite certificado de rede do usuário para testes whitehat";
+    public static String REMOVE_BUILD_EXPIRE_POPUP = "Remover popup de versão expirada";
+    public static String REMOVE_BUILD_EXPIRE_POPUP_DESC = "Remove o popup que aparece após um tempo quando a versão do app envelhece";
+    public static String EXPORT_DEV_OVERRIDES = "Exportar Backup";
+    public static String IMPORT_DEV_OVERRIDES = "Importar Backup";
+    public static String IMPORT_ID_MAPPING = "Importar arquivos de mapeamento";
+    public static String DOWNLOAD_ID_MAPPING = "Baixar arquivo de mapeamento";
+
+    public static String CATEGORY_LINKS = "Links";
+    public static String OPEN_LINKS_EXTERNALLY = "Abrir links externamente";
+    public static String OPEN_LINKS_EXTERNALLY_DESC = "Abrir links no navegador externo em vez do navegador interno do app";
+    public static String SANITIZE_SHARE_LINKS = "Higienizar links compartilhados";
+
+    public static String CATEGORY_GHOST = "Fantasma";
+    public static String VIEW_STORIES_ANONYMOUSLY = "Ver stories anonimamente";
+    public static String VIEW_LIVE_ANONYMOUSLY = "Ver lives anonimamente";
+    public static String DISABLE_TYPING_STATUS = "Desativar status de digitação";
+    public static String DISABLE_SCREENSHOT_DETECTION = "Desativar detecção de captura de tela";
+    public static String VIEW_DM_ANONYMOUSLY = "Ver mensagens diretas anonimamente";
+
+    public static String CATEGORY_DISTRACTION_FREE = "Sem distrações";
+    public static String DISABLE_STORIES = "Desativar stories";
+    public static String DISABLE_HIGHLIGHTS = "Desativar destaques";
+    public static String DISABLE_EXPLORE = "Desativar pesquisa";
+    public static String DISABLE_COMMENTS = "Desativar comentários";
+    public static String LIMIT_FOLLOWING_FEED = "Limitar feed aos perfis seguidos";
+    public static String LIMIT_FOLLOWING_FEED_DESC = "Filtra o feed inicial para mostrar apenas conteúdo dos perfis que você segue.";
+    public static String DISABLE_REELS_SCROLLING = "Desativar rolagem dos Reels";
+    public static String DISABLE_REELS_SCROLLING_DESC =
+            "Desativa a rolagem infinita nos Reels do Instagram, impedindo deslizar para o próximo Reel. " +
+            "Nota: Em uma instalação limpa, a animação de dica pode aparecer, mas irá parar sozinha após alguns segundos.";
+    public static String HIDE_STORIES_TRAY = "Ocultar barra de stories";
+    public static String HIDE_STORIES_TRAY_DESC = "Oculta a barra de stories do feed principal";
+    public static String HIDE_NOTES_TRAY = "Ocultar barra de notas";
+    public static String HIDE_NOTES_TRAY_DESC = "Oculta a barra de notas na seção de mensagens";
+    public static String HIDE_GROUP_CREATION_BUTTON_ON_SHARESHEET = "Ocultar botão de criação de grupo na aba de compartilhamento";
+    public static String DISABLE_DOUBLE_TAP_LIKE_POST = "Desativar curtida por toque duplo em posts";
+    public static String DISABLE_DOUBLE_TAP_LIKE_REEL = "Desativar curtida por toque duplo em reels";
+    public static String DISABLE_DOUBLE_TAP_LIKE_COMMENT = "Desativar curtida por toque duplo em comentários";
+    public static String DISABLE_DOUBLE_TAP_LIKE_MESSAGE = "Desativar curtida por toque duplo em mensagens";
+
+    public static String CATEGORY_MISC = "Diversos";
+    public static String DISABLE_ANALYTICS = "Desativar análises";
+    public static String DELETE_ANALYTICS_CACHE = "Excluir cache de análises";
+    public static String DISABLE_ANALYTICS_DESC = "Bloqueia análises enviados para os servidores do Instagram/Facebook";
+    public static String DISABLE_DISCOVER_PEOPLE = "Desativar descobrir pessoas";
+    public static String DISABLE_DISCOVER_PEOPLE_DESC = "Desativar a seção descobrir pessoas no perfil";
+    public static String FOLLOW_BACK_INDICATOR = "Ativar indicador de seguindo de volta";
+    public static String FBI_FOLLOWS_YOU = "Segue você";
+    public static String FBI_DOESNT_FOLLOWS_YOU = "Não segue você";
+    public static String VIEW_STORY_MENTIONS = "Ver menções no story";
+    public static String VSM_TITLE = "Menções no story";
+    public static String VSM_NO_MENTIONS = "Nenhuma menção neste story";
+    public static String DISABLE_STORY_FLIPPING = "Desativar troca automática de stories";
+    public static String DISABLE_STORY_FLIPPING_DESC = "Desativa ir para o próximo story automaticamente";
+    public static String DISABLE_VIDEO_AUTOPLAY = "Desativar reprodução automática de vídeo";
+    public static String STORIES_AUDIO_AUTOPLAY = "Reprodução automática de áudio nos stories";
+    public static String CUSTOMISE_STORY_TIMESTAMP = "Personalizar horário do story";
+    public static String CUSTOMISE_STORY_TIMESTAMP_DESC = "Personaliza o horário mostrado de quando o story foi postado";
+    public static String UNLIMITED_REPLAYS = "Tornar mídia permanente";
+    public static String UNLIMITED_REPLAYS_DESC = "Transforma mídias de visualização única ou dupla em visualização permanente";
+    public static String IMPROVE_IMAGE_VIEWING = "Melhorar qualidade de imagens";
+    public static String IMPROVE_IMAGE_VIEWING_DESC = "Obtém imagens na resolução máxima do servidor";
+    public static String HIDE_RESHARE_BUTTON = "Ocultar botão de repostar";
+    public static String COPY_COMMENT = "Copiar comentário";
+    public static String COPY_COMMENT_DESC = "Adiciona um botão para copiar comentários em posts e reels";
+    public static String COMMENT_COPIED_SUCCESS = "Comentário copiado";
+    public static String COMMENT_COPIED_FAILED = "Nenhum texto encontrado para copiar";
+    public static String COPY_USERNAME = "Copiar nome de usuário";
+    public static String COPY_FULL_NAME = "Copiar nome completo";
+    public static String COPY_USER_ID = "Copiar ID do usuário";
+    public static String COPY_BIO = "Copiar bio";
+    public static String DOWNLOAD_PROFILE_PICTURE = "Baixar foto de perfil";
+    public static String COPIED = "Copiado";
+    public static String MORE_PROFILE_OPTIONS = "Mais opções de perfil";
+    public static String REMOVE_EMPTY_BOTTOM_SPACE = "Remover espaço vazio inferior";
+    public static String UNLOCK_PLUS_BENEFITS = "Desbloquear benefícios Plus";
+    public static String UNLOCK_PLUS_BENEFITS_DESC = "Desbloqueia benefícios da assinatura 'Plus' verificados localmente. USE POR SUA CONTA E RISCO";
+    public static String CUSTOMISE_STORY_RING_SIZE = "Personalizar tamanho do anel do story";
+
+    public static String CATEGORY_DOWNLOAD_MEDIA = "Baixar mídia";
+    public static String ENABLE_DOWNLOAD = "Ativar download";
+    public static String ENABLE_DIRECT_DOWNLOAD = "Ativar download direct";
+    public static String ENABLE_DIRECT_DOWNLOAD_DESC = "Baixa a mídia atual sem pedir opções";
+    public static String DOWNLOAD_USERNAME_FOLDER = "Separar mídias por nome de usuário";
+    public static String DOWNLOAD_USERNAME_FOLDER_DESC = "Cria pastas baseadas no nome de usuário";
+    public static String DOWNLOAD_CURRENT_MEDIA = "Baixar mídia atual";
+    public static String DOWNLOAD_AS_IMAGE = "Baixar como imagem";
+    public static String DOWNLOAD_AUDIO = "Baixar áudio";
+    public static String DOWNLOAD_OPTIONS = "Opções de download";
+    public static String COPY_MEDIA_LINK = "Copiar link da mídia";
+    public static String COPIED_MEDIA_LINK = "Link da mídia copiado";
+    public static String DOWNLOAD_ALL = "Baixar tudo";
+    public static String DOWNLOADING_MEDIA = "Baixando : ";
+    public static String DOWNLOADED_MEDIA = "Baixado : ";
+    public static String MEDIA_EXISTS = "A mídia já existe";
+    public static String DOWNLOAD_FAILED_MEDIA = "Falha ao baixar : ";
+    public static String POST_OPTIONS = "Opções do post";
+    public static String COPY_POST_DESCRIPTION = "Copiar descrição do post";
+    public static String COPY_POST_OWNER_USERNAME = "Copiar usuário do dono do post";
+    public static String COPY_POST_OWNER_FULLNAME = "Copiar nome completo do dono do post";
+    public static String ENABLE_MORE_OPTIONS_ON_POST = "Ativar mais opções no post";
+    public static String ENABLE_MORE_OPTIONS_ON_POST_DESC = "Obtém mais opções ao segurar o post";
+
+    public static String CATEGORY_HIDE_NAVIGATION_BUTTONS = "Ocultar botões de navegação";
+    public static String HIDE_NAVIGATION_FEED = "Ocultar botão Feed";
+    public static String HIDE_NAVIGATION_REELS = "Ocultar botão Reels";
+    public static String HIDE_NAVIGATION_DIRECT = "Ocultar botão Direct";
+    public static String HIDE_NAVIGATION_SEARCH = "Ocultar botão Pesquisa";
+    public static String HIDE_NAVIGATION_CREATE = "Ocultar botão Criar";
+
+    public static String PATCH_INFO_TITLE = "Informações do patch";
+    public static String EXPORT_PIKO_PREF = "Exportar preferências do Piko";
+    public static String IMPORT_PIKO_PREF = "Importar preferências do Piko";
+    public static String APP_VERSION = "Versão do app: %s";
+    public static String PATCH_VERSION = "Versão do patch: %s";
+
+    public static String EXPORT_SUCCESS = "Exportado com sucesso";
+    public static String EXPORT_FAIL = "Falha ao exportar";
+    public static String FAIL_NO_PATH = "Nenhum caminho de destino fornecido";
+    public static String FAIL_NO_FILE = "Arquivo não encontrado";
+    public static String IMPORT_SUCCESS = "Importado com sucesso";
+    public static String IMPORT_FAIL = "Falha na importação";
+    public static String RESTART_APP = "O app precisa ser reiniciado para as alterações serem feitos";
+    public static String OK = "OK";
+    public static String DELETED = "Excluído";
+    public static String PIKO_DEBUG = "Debug do Piko";
+    public static String PIKO_DEBUG_DESC = "Adiciona opções de debug em alguns componentes para testes";
+    public static String PIKO_EXPORT_EXPERIMENT_LIST = "Exportar lista de experimentos Piko";
+    public static String PIKO_EXPORT_EXPERIMENT_MAPPINGS = "Exportar mapeamentos de experimentos Piko";
+
+    public static String OPEN_IMAGE_EXTERNALLY = "Abrir imagem externamente";
+    public static String OPEN_VIDEO_EXTERNALLY = "Abrir vídeo externamente";
+    public static String OPEN_IMAGE_WITH = "Abrir imagem com";
+    public static String OPEN_VIDEO_WITH = "Abrir vídeo com";
+
+    public static String CHANGE_LIKE_ANIMATION = "Alterar animação de curtida";
+    public static String CHANGE_LIKE_ANIMATION_DESC = "Altera a animação para uma das animações existentes de criadores rings";
+    public static String AVAILABLE_LIKE_ANIMATION = "Animações de curtida disponíveis";
+
+    public static String DEFAULT = "Padrão";
+    public static String ARR_DETAILED_TIMESTAMP = "Horário detalhado";
+    public static String ARR_TIME_LEFT = "Tempo restante";
+
+    public static String WELCOME_TITLE = "Bem-vindo ao Piko";
+    public static String WELCOME_MESSAGE = "Para melhorar sua experiência com o Piko, visite as configurações do Piko e ajuste suas configurações";
+    public static String GOTO_PIKO_SETTINGS = "Ir para configurações do Piko";
+    public static String NO_INTERNET = "Conecte-se à internet";
+}
